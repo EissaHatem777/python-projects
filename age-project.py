98ea09be9f05e10fac85cfabaac3195d08685044
EissaHatem777 <abac44@gmail.com>
2023-11-04 21:52:14 +0600

    Pyhton age group project

---
+++ age-project.py
@@ -0,0 +1,12 @@
+def age_group(age):
+  if age<18:
+    return "Minor"
+  elif age>=18 and age <=30:
+    return "Adult"
+  elif age>30:
+    return "Senior"
+  else:
+    return "Invalid"
+age=input(int("Enter your age: "))
+age_group=age_group(age)
+print(f"You are {age_group}")  
